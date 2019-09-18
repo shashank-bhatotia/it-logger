6b87cc0f0bec6a9714e03994a565c2d19dbe1e17
shashank <shashankbhatotia@gmail.com>
2019-09-18 08:20:18 +0530

    Add scripts and materialize-css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js'
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       It logger
